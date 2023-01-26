590ce211647fad619457556dbfd81609ca93727a
kadir <idia_2008@hotmail.com>
2023-01-26 23:42:31 +0300

    create the spinner component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,14 +16,14 @@ export default function Header() {
       }
     });
   }, [auth]);
-  
+
   function pathMatchRoute(route) {
     if (route === location.pathname) {
       return true;
     }
   }
   return (
-    <div className="bg-white border-b shadow-sm sticky top-0 z-50">
+    <div className="bg-white border-b shadow-sm sticky top-0 z-40">
       <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
         <div>
           <img
