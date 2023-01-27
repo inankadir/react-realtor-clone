7e2498bea2d15b192ff3fe31b3db0c8505769b77
kadir <idia_2008@hotmail.com>
2023-01-27 16:44:07 +0300

    add functionality including create listing page

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     <>
       <Router>
         <Header />
-        <Routes path="/profile" element={<PrivateRoute/>}>
+        <Routes path="/profile" element={<PrivateRoute />}>
           <Route path="/" element={<Home />} />
           <Route>
             <Route path="/profile" element={<Profile />} />
@@ -25,7 +25,9 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/offers" element={<Offers />} />
-          <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="create-listing" element={<PrivateRoute />}>
+            <Route path="/create-listing" element={<CreateListing />} />
+          </Route>
         </Routes>
       </Router>
       <ToastContainer
